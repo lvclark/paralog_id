9af7166e13a4ebf02a96ea6a17ec806661b6e321
Lindsay V. Clark <lvclark@illinois.edu>
2019-06-07 15:46:14 -0500

    throw out simulated annealing

--- scripts/isoloci_fun_test.py
+++ scripts/isoloci_fun_test.py
@@ -1,4 +1,4 @@
-from isoloci_fun import GiniSimpson, HindHe, SwapHap, InitHapAssign, \
+from isoloci_fun import GiniSimpson, HindHe, InitHapAssign, \
 MeanNMperLoc, IndexHapAssign, AlleleAssociations, AdjustHapAssignByAlAssociations, \
 FindNeighbors
 
@@ -15,17 +15,11 @@ print(HindHe([[10, 5, 0, 0, 0],
 myNM = [[4, 5, 7, 1, 6, 0, 8, 3, 2, 3, 2, 1, 2, 2, 2, 2, 5, 2, 2, 2, 1, 2, 3, 2, 5, 5, 11, 2, 2, 3, 4, 5],
         [8, 1, 7, 5, 2, 4, 9, 4, 3, 4, 3, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 4, 3, 2, 2, 8, 1, 3, 0, 1, 2]]
 nhap = len(myNM[0])
-#myAssign = [[i for i in range(nhap) if myNM[0][i] < myNM[1][i]],
-#            [i for i in range(nhap) if myNM[0][i] >= myNM[1][i]]]
+
 myAssign = InitHapAssign(myNM)
 print(myAssign)
 
 print(MeanNMperLoc(myNM, myAssign))
-
-for i in range(10):
-  myAssign = SwapHap(myNM, myAssign, 80, [{1,4}, {10, 11, 12, 13}], 0.01)
-  print(myAssign)
-  print(IndexHapAssign(myAssign))
 
 print(MeanNMperLoc(myNM, myAssign))
 
@@ -40,7 +34,7 @@ print(IndexHapAssign([[2,1],[0]]))
 print(AlleleAssociations([[10, 5, 0, 0, 0],
               [3, 0, 10, 0, 0],
               [0, 3, 0, 20, 0]]))
-              
+
 print(AdjustHapAssignByAlAssociations([{2,3,4}, {1,5}], \
 [[0,1,2],[3,4,5]]))
 
